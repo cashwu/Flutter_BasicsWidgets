cde333586b1c44cba45bd537b0f66b77933c1738
Cash Wu <cash@cashwu.com>
2019-01-01 17:20:42 +0800

    split code to another class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'product_manager.dart';
 
 main() => runApp(MyApp());
 
@@ -10,42 +11,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
   @override
   build(context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("EasyList"),
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                   _products.add('Adv Food Tester');
-                  });
-                },
-                child: Text("Add Product"),
-              ),
-            ),
-            Column(
-                children: _products
-                    .map((element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset("assets/food.jpg"),
-                              Text(element)
-                            ],
-                          ),
-                        ))
-                    .toList())
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("EasyList"),
+          ),
+          body: ProductManager('Food Tester')),
     );
   }
 }
---
+++ lib/product_manager.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course/products.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
+  @override
+  _ProductManagerState createState() => _ProductManagerState();
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: RaisedButton(
+          onPressed: () {
+            setState(() {
+              _products.add('Adv Food Tester');
+            });
+          },
+          child: Text("Add Product"),
+        ),
+      ),
+      Products(_products)
+    ]);
+  }
+}
