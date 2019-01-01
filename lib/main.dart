e4870045ae3ef83219fc4eee94c5b9db354537aa
Cash Wu <cash@cashwu.com>
2019-01-01 18:43:08 +0800

    modify theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,11 @@ class _MyAppState extends State<MyApp> {
   @override
   build(context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text("EasyList"),
